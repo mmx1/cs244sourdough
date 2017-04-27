03551aecbdb59f86c4adc6a23ef7e0e87ba46b5f
Keith Major <keithm2@stanford.edu>
2017-04-27 09:25:09 -0700

    Power 27 using min_rtt for thresholds

--- datagrump/controller.cc
+++ datagrump/controller.cc
@@ -40,6 +40,7 @@ void Controller::datagram_was_sent( const uint64_t sequence_number,
 }
 
 unsigned int prev_RTT = 0;
+unsigned int min_RTT = 0 - 1;
 unsigned int overload_wnd = 0;
 unsigned int old_window_size = 0;
 
@@ -53,10 +54,12 @@ void Controller::ack_received( const uint64_t sequence_number_acked,
 			       const uint64_t timestamp_ack_received )
                                /* when the ack was received (by sender) */
 {
+  //cerr << sequence_number_acked << endl;
+  //cerr << recv_timestamp_acked << endl;
   unsigned int curr_RTT = timestamp_ack_received - send_timestamp_acked;
   if (overload_wnd){
     overload_wnd--;
-    cerr << overload_wnd << endl;
+    //cerr << overload_wnd << endl;
     // if (!overload_wnd){
     //   the_window_size = old_window_size;
     // }
@@ -66,51 +69,45 @@ void Controller::ack_received( const uint64_t sequence_number_acked,
     
   if(prev_RTT) {
     if (prev_RTT < curr_RTT) {
-      if (curr_RTT > 300){
-        cerr << "300 cut"<< endl;
+      if (curr_RTT > (min_RTT * 7)){
+        //cerr << "*3 cut"<< endl;
         overload_wnd = the_window_size;
-        old_window_size = the_window_size / 2;
+        //old_window_size = the_window_size / 2;
         the_window_size = 0;
-      } else if (curr_RTT - prev_RTT > 25){
-        cerr << "25 cut" << endl;
+        //part_of_window = 0;
+      } else if (curr_RTT - prev_RTT > (min_RTT / 1.5)){
+        //cerr << "/2 cut" << endl;
         overload_wnd = the_window_size;
         the_window_size = the_window_size / 2;
-      } else if (curr_RTT - prev_RTT > 15 || curr_RTT > 90){
+        //part_of_window = 0;
+      } else if (curr_RTT - prev_RTT > (min_RTT / 2.5) || curr_RTT > (min_RTT*2)){
         the_window_size = (the_window_size == 0) ? 0 : the_window_size - 1;
+        //cerr << "slow reduce" << endl;
+        //part_of_window = 0;
       }else {
         part_of_window ++;
         if (part_of_window >= (the_window_size)){
           the_window_size++;
           part_of_window = 0;
         }
+        //cerr << "slow grow" << endl;
       }
       
     } else {
-      part_of_window +=2;
+      part_of_window +=3;
       if (part_of_window >= (the_window_size)){
         the_window_size++;
         part_of_window = 0;
       }
+      //cerr << "fast grow" << endl;
     }
   } 
   prev_RTT = curr_RTT;
-
-  /* Default: take no action */
-  // cerr << "send_timestamp_acked: " << send_timestamp_acked << endl;
-  // cerr << "recv_timestamp_acked: " << recv_timestamp_acked << endl;
-  // cerr << "timestamp_ack_received: " << timestamp_ack_received << endl;
-  // if (timestamp_ack_received - send_timestamp_acked > 100){
-  //   the_window_size = the_window_size/2 ;
-  //   //cerr << "Halfed "<< endl;
-  //   part_of_window = 0;
-  // } else {
-  //   part_of_window ++;
-  //   if (part_of_window >= (the_window_size)){
-  //     the_window_size++;
-  //     part_of_window = 0;
-  //   }
-  //   //cerr << "Additive " << endl;
-  // }
+  if(curr_RTT < min_RTT){
+    min_RTT = curr_RTT;
+  }
+  // cerr << "Min RTT: " << min_RTT << endl;
+  // cerr << "window size: " << the_window_size << endl;
   if ( debug_ ) {
     cerr << "At time " << timestamp_ack_received
 	 << " received ack for datagram " << sequence_number_acked
